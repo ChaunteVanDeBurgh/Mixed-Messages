a05df19d1f132425112d34213680f6ad27b2a901
Chaunte <chauntevandeburgh@gmail.com>
2023-02-22 12:05:36 -0600

    create functin to randomly sellect words

---
+++ messageDATA.js
@@ -0,0 +1,14 @@
+//parts of speach with words to make messages with
+const noun = ['cat','dog','rat','fecal specamin','tranny', 'tweeker','swinger','pinapple', 'shoe', 'skateboard'];
+const verb = ['ran', 'jumpped', 'vomited', 'swam','exploded','combusted','raced', 'twitched', 'injured','spoke'];
+const adj = ['red', 'blue','green','big','small','wet', 'dry', 'angry','sad','dominant'];
+const adv = ['slowly', 'quickly', 'modestly', 'truelly', 'decidedly', 'cutely', 'nastaly', 'crazily', 'naturally', 'generously']; 
+
+const makeMessage = (noun, verb, adj, adv) => {
+  const chooseRandomWord = (words) => {
+    return words[Math.floor(Math.random() * words.length)];
+  }
+  return `The ${chooseRandomWord(adj)} ${chooseRandomWord(noun)} ${chooseRandomWord(verb)} ${chooseRandomWord(adv)}.`;
+}
+
+console.log(makeMessage(noun, verb, adj, adv));
