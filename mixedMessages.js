bc659eaaa9d76525bcb87477dbd761bcb0dc8881
Chaunte <chauntevandeburgh@gmail.com>
2023-02-22 12:03:53 -0600

    create arrays of words sorted in parts of speech

---
+++ mixedMessages.js
@@ -0,0 +1,5 @@
+//parts of speach with words to make messages with
+const noun = ['cat','dog','rat','fecal specamin','tranny', 'tweeker','swinger','pinapple', 'shoe', 'skateboard'];
+const verb = ['ran', 'jumpped', 'vomited', 'swam','exploded','combusted','raced', 'twitched', 'injured','spoke'];
+const adj = ['red', 'blue','green','big','small','wet', 'dry', 'angry','sad','dominant'];
+const adv = ['slowly', 'quickly', 'modestly', 'truelly', 'decidedly', 'cutely', 'nastaly', 'crazily', 'naturally', 'generously']; 
